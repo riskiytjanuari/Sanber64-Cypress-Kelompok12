29f5a30df57a30130ee3944a672f0a68396912bc
Haris Rifani <amdharisrfn16@gmail.com>
2025-02-22 22:52:11 +0800

    add register feature

---
+++ cypress/support/page-object/registerPage.js
@@ -0,0 +1,30 @@
+class RegisterPage {
+    firstName = '[name="firstname"]'
+    lastName = '[name="lastname"]'
+    email = '[name="email"]'
+    password = '#password'
+    pass_confirm = '[name="password_confirmation"]'
+    registerButton = 'button[title="Create an Account"]'
+
+    verifyOnRegisterPage() {
+        cy.url().should('include', 'customer/account/create')
+    }
+
+    inputFirstName(firstName) {
+        cy.get(this.firstName).type(firstName)
+    }
+
+    fillRegistrationForm(firstName, lastName, email, password) {
+        cy.get(this.firstName).type(firstName)
+        cy.get(this.lastName).type(lastName)
+        cy.get(this.email).type(email)
+        cy.get(this.password).type(password)
+        cy.get(this.pass_confirm).type(password)
+    }
+
+    submitForm() {
+        cy.get(this.registerButton).click()
+    }
+}
+
+export default new RegisterPage()
